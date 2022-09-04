0768bf527b85509ec922599b912e021fc7af17ec
McCrearyD <mccreary@dyllan.ai>
2022-09-04 14:44:02 -0700

    implement update game tree

--- fmc.py
+++ fmc.py
@@ -16,49 +16,27 @@ def _relativize_vector(vector):
     standard[standard <= 0] = torch.exp(standard[standard <= 0])
     return standard
 
-
-def _update_game_tree(game_tree: nx.DiGraph, walker_node_ids: torch.Tensor, actions, rewards, clone_partners, clone_mask, dynamics_model):
-    num_walkers = dynamics_model.state.shape[0]
-
-    candidate_walker_ids = torch.randint(99999, 999999999999, (num_walkers,))
-
-    for walker_index in range(num_walkers):
-        cloned = clone_mask[walker_index]
-
-        if cloned:
-            # TODO: prune tree (remove all nodes/edges before)
-            continue
-
-        previous_node = walker_node_ids[walker_index]
-        new_node = candidate_walker_ids[walker_index]
-
-        game_tree.add_node(new_node, state=dynamics_model.state[walker_index])
-        game_tree.add_edge(previous_node, new_node, action=actions[walker_index], weight=rewards[walker_index])
-
-    # clone new node ids
-    walker_node_ids[clone_mask] = candidate_walker_ids[clone_partners[clone_mask]]
-
-
 class FMC:
     def __init__(self, num_walkers: int, dynamics_model, initial_state, balance: float = 1, verbose: bool = False):
         self.num_walkers = num_walkers
         self.balance = balance
         self.verbose = verbose
 
+        self.state = torch.zeros((num_walkers, *initial_state.shape))
+        self.state[:] = initial_state
+
         self.root = 0
         self.game_tree = nx.DiGraph()
         self.game_tree.add_node(self.root, state=initial_state)
-
-        self.state = torch.zeros((num_walkers, *initial_state.shape))
-        self.state[:] = initial_state
+        self.walker_node_ids = torch.zeros(self.num_walkers, dtype=torch.long)
 
         self.dynamics_model = dynamics_model
         self.dynamics_model.set_state(self.state)
 
     @torch.no_grad()
     def _perturbate(self):
-        actions = self._get_actions()
-        self.rewards = self.dynamics_model.forward(actions)
+        self._assign_actions()
+        self.rewards = self.dynamics_model.forward(self.actions)
 
     @torch.no_grad()
     def simulate(self, k: int):
@@ -66,22 +44,25 @@ class FMC:
         for _ in range(k):
             self._perturbate()
             self._clone_states()
-            # _update_game_tree(game_tree, walker_node_ids, actions, rewards, clone_partners, clone_mask, dynamics_model)
+            self._update_game_tree()
+
+            # nx.draw(self.game_tree)
+            # plt.show()
 
         # sanity check
         assert self.dynamics_model.state.shape == (self.num_walkers, self.dynamics_model.embedding_size)
 
-        return self._get_actions()[0, 0].numpy()  # TODO!
+        return self.actions[0, 0].numpy()  # TODO select the actual best action
 
     @torch.no_grad()
-    def _get_actions(self):
+    def _assign_actions(self):
         # TODO: policy function
         
         actions = []
         for _ in range(self.num_walkers):
             action = self.dynamics_model.action_space.sample()
             actions.append(action)
-        return torch.tensor(actions).unsqueeze(-1)
+        self.actions = torch.tensor(actions).unsqueeze(-1)
 
     @torch.no_grad()
     def _assign_clone_partners(self):
@@ -134,3 +115,27 @@ class FMC:
 
         if self.verbose:
             print("state after", self.dynamics_model.state)
+
+    @torch.no_grad()
+    def _update_game_tree(self):
+        candidate_walker_ids = torch.randint(1, 9999999, (self.num_walkers,))
+
+        for walker_index in range(self.num_walkers):
+            cloned = self.clone_mask[walker_index]
+
+            if cloned:
+                # TODO: prune tree (remove all nodes/edges before)
+                continue
+
+            previous_node = self.walker_node_ids[walker_index].item()
+            new_node = candidate_walker_ids[walker_index].item()
+
+            self.game_tree.add_node(new_node, state=self.dynamics_model.state[walker_index])
+            self.game_tree.add_edge(previous_node, new_node, action=self.actions[walker_index], weight=self.rewards[walker_index])
+
+        candidate_walker_ids[self.clone_mask] = candidate_walker_ids[self.clone_partners[self.clone_mask]]
+        self.walker_node_ids[:] = candidate_walker_ids
+
+        # clone new node ids
+        # self.walker_node_ids[:] = candidate_walker_ids
+        # self.walker_node_ids[self.clone_mask] = candidate_walker_ids[self.clone_partners[self.clone_mask]]
