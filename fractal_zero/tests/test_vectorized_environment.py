8e83530441c96f533fd7326c74ed18f2c4bafdf7
nollied <dyllanmccreary@gmail.com>
2022-09-21 14:40:10 -0700

    begin testing step

--- fractal_zero/tests/test_vectorized_environment.py
+++ fractal_zero/tests/test_vectorized_environment.py
@@ -5,7 +5,7 @@ from fractal_zero.models.dynamics import FullyConnectedDynamicsModel
 from fractal_zero.models.joint_model import JointModel
 from fractal_zero.models.prediction import FullyConnectedPredictionModel
 from fractal_zero.models.representation import FullyConnectedRepresentationModel
-from fractal_zero.vectorized_environment import RayVectorizedEnvironment, VectorizedDynamicsModelEnvironment, load_environment
+from fractal_zero.vectorized_environment import RayVectorizedEnvironment, VectorizedDynamicsModelEnvironment, VectorizedEnvironment, load_environment
 
 
 def _build_test_joint_model(env) -> JointModel:
@@ -18,6 +18,14 @@ def _build_test_joint_model(env) -> JointModel:
     return JointModel(rep, dyn, pred)
 
 
+def _test_step(vec_env: VectorizedEnvironment):
+    actions = vec_env.batched_action_space_sample()
+    obss, rews, dones, infos = vec_env.batch_step(actions)
+
+    assert len(obss) == vec_env.n
+    assert len(obss) == len(rews) == len(dones) == len(infos)
+
+
 def test_vectorized_cartpole_ray():
     n = 2
 
@@ -26,18 +34,20 @@ def test_vectorized_cartpole_ray():
     initial_observations = vec_env.batch_reset()
     assert len(initial_observations) == n
 
-    actions = vec_env.batched_action_space_sample()
-    obss, rews, dones, infos = vec_env.batch_step(actions)
+    # TODO: test set all states
 
-    assert len(obss) == n
-    assert len(obss) == len(rews) == len(dones) == len(infos)
+    _test_step(vec_env)
 
 
 def test_vectorized_cartpole_dynamics_model():
     n = 2
 
-    joint_model = _build_test_joint_model("CartPole-v0")
-    vec_env = VectorizedDynamicsModelEnvironment("CartPole-v0", n=n, joint_model=joint_model)
+    env = load_environment("CartPole-v0")
 
-    print(vec_env)
+    joint_model = _build_test_joint_model(env)
+    vec_env = VectorizedDynamicsModelEnvironment(env, n=n, joint_model=joint_model)
 
+    obs = env.reset()
+    vec_env.set_all_states(env, obs)
+
+    _test_step(vec_env)
