182048352b6ce0ec16b7e0d5b4ebf66b09be5192
McCrearyD <mccreary@dyllan.ai>
2022-09-25 13:24:33 -0700

    train on batch

--- fractal_zero/trainers/online.py
+++ fractal_zero/trainers/online.py
@@ -13,10 +13,12 @@ class OnlineFMCPolicyTrainer:
     weights are used during the search process. So the data is generated, the model is trained, and the cycle continues.
     """
 
-    def __init__(self, env: Union[str, gym.Env], policy_model: torch.nn.Module, num_walkers: int):
+    def __init__(self, env: Union[str, gym.Env], policy_model: torch.nn.Module, optimizer: torch.optim.Optimizer, num_walkers: int):
         self.env = load_environment(env)
         self.vec_env = RayVectorizedEnvironment(env, num_walkers)
+
         self.policy_model = policy_model
+        self.optimizer = optimizer
 
     def _get_best_history(self) -> GameHistory:
         return max(self.fmc.game_histories, key=lambda h: h.total_reward)
@@ -26,16 +28,27 @@ class OnlineFMCPolicyTrainer:
 
         x = torch.stack(history.observations[1:]).float()
         t = torch.tensor(history.actions[1:])
-        return x, t
+        return x, t.long()
 
-    def train_epsiode(self, max_steps: int):
+    def generate_episode_data(self, max_steps: int):
         self.vec_env.batch_reset()
 
         self.fmc = FMC(self.vec_env, self.policy_model)
         self.fmc.simulate(max_steps)
 
+    def train_on_latest_episode(self):
+        self.policy_model.train()
+        self.optimizer.zero_grad()
+
         x, t = self._get_batch()
-        print(x.shape, t.shape)
+        y = self.policy_model.forward(x)
+
+        loss = F.cross_entropy(y, t)
+        loss.backward()
+
+        self.optimizer.step()
+
+        return loss.item()
 
         # game_history_weights = F.softmax(self.fmc.visit_buffer.float(), dim=0)
 
