aaeaf3e4a610beea9341c8d6989f11988da35935
McCrearyD <mccreary@dyllan.ai>
2022-09-30 17:20:21 -0700

    add param for weight_type, also skip node if no actions

--- fractal_zero/data/tree_sampler.py
+++ fractal_zero/data/tree_sampler.py
@@ -1,27 +1,41 @@
 import torch
+import numpy as np
 
-from fractal_zero.search.tree import GameTree
+from fractal_zero.search.tree import GameTree, StateNode
 
 
 class TreeSampler:
-    def __init__(self, tree: GameTree, sample_type: str="all_nodes"):
+    def __init__(self, tree: GameTree, sample_type: str="all_nodes", weight_type: str="walker_children_ratio"):
         self.tree = tree
-
-        # TODO: parameter for weighting type (constant, walker_vists/clone_receives, walker_children, etc.)
-
         self.sample_type = sample_type
+        self.weight_type = weight_type
+
+        if not self.tree.prune:
+            raise NotImplementedError("TreeSampling on an unpruned tree has not been considered.")
+
+    def _calculate_weight(self, node: StateNode) -> float:
+        if self.weight_type == "walker_children_ratio":
+            return node.num_child_walkers / self.tree.num_walkers
+        elif self.weight_type == "constant":
+            return 1.0
+        elif self.weight_type == "time_spent_at_node":
+            raise NotImplementedError
+        raise ValueError(f"{self.weight_type} not supported.")
 
     def _get_best_path_as_batch(self):
         observations = []
         actions = []
+        weights = []
 
         path = self.tree.best_path
         for state, action in path:
-            # obs = torch.tensor(state.observation)
+            # NOTE: never skip due to weight!
+            weight = self._calculate_weight(state)
+            weights.append([weight])
+
             observations.append(state.observation)
             actions.append([action])
 
-        weights = torch.ones((1, 1, len(observations))).float()
         return observations, actions, weights
 
     def _get_all_nodes_batch(self):
@@ -31,18 +45,25 @@ class TreeSampler:
 
         g = self.tree.g
         for node in g.nodes:
-            observations.append(node.observation)
 
             actions = []
             weights = []
             for _, child_node, data in g.out_edges(node, data=True):
-                weight = child_node.num_child_walkers / self.tree.num_walkers
+                weight = self._calculate_weight(child_node)
+
+                # skip if the weight is almost 0.
+                if np.isclose(weight, 0):
+                    continue
 
                 weights.append(weight)
-
                 action = data["action"]
                 actions.append(action)
 
+            # if no action targets exist, skip this state.
+            if len(actions) <= 0:
+                continue
+
+            observations.append(node.observation)
             child_actions.append(actions)
             child_weights.append(torch.tensor(weights).float())
 
