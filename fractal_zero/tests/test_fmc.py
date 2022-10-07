f69b80ffcb0e33fe72d5a0608ff7f1861e06cbaa
dyllan2 <dyllan@allegrolabs.io>
2022-10-07 11:52:22 -0700

    update test to exacerbate problem

--- fractal_zero/tests/test_fmc.py
+++ fractal_zero/tests/test_fmc.py
@@ -25,11 +25,22 @@ def _assert_mean_total_rewards(
     for _ in range(trials):
         fmc.reset()
 
-        # 200 is the max reward accumulate-able in cartpole.
         fmc.simulate(steps, use_tqdm=use_tqdm)
 
         total_reward = fmc.tree.best_path.total_reward
         total_rewards.append(total_reward)
+
+        # cartpole has a max reward of 200.
+        assert (fmc.scores <= 200).all()
+
+        # TODO: make sure the tree's best path gets the best walker's same score.
+        best_walker_index = fmc.scores.argmax()
+        score = fmc.scores[best_walker_index]
+
+        expected_best_walker_path = fmc.tree.walker_paths[best_walker_index]
+        assert np.isclose(score, expected_best_walker_path.total_reward)
+        assert expected_best_walker_path == fmc.tree.best_path
+
     assert np.mean(total_rewards) > expected_mean_reward
 
 
@@ -65,4 +76,6 @@ def test_cartpole_consistently_high_reward():
     n = 64
     vec_env = SerialVectorizedEnvironment("CartPole-v0", n=n)
     fmc = FMC(vec_env, balance=1)
+
+    # 200 is the max reward accumulate-able in cartpole.
     _assert_mean_total_rewards(fmc, 400, 140, use_tqdm=True)
